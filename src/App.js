7d381e87af3a04af2cd21285b6b9b8155f68f033
michalholder <michalholder710@gmail.com>
2021-05-10 17:39:24 +0300

    reactApp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import{BrowserRouter as Router,Switch , Route, Link} from 'react-router-dom';
+import{HashaRouter as Router,Switch , Route, Link} from 'react-router-dom';
 import Tittel from './components/Tittel';
 import SmartHouse from './components/SmartHouse';
 import AddAroom from './components/AddAroom';
